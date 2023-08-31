e0a442b03fa38cd138eac432eea5c767e6bbf01f
Peter Sooley <peter.sooley@gmail.com>
2023-08-30 21:22:27 -0700

    playing with bubbles

--- main.go
+++ main.go
@@ -1,16 +1,69 @@
 package main
 
 import (
-	"elmdash/gothru/maze"
 	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
-func main() {
-	maze := maze.Generate(5, 11111)
-	for from, to := range maze.Path() {
-		for t := range to {
-			fmt.Printf("expectConnection(t, path, maze.Cell{%v, %v}, maze.Cell{%v, %v})\n", from.X, from.Y, t.X, t.Y)
+type model struct {
+	cursor int
+}
+
+func newModel() model {
+	return model{}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	// Is it a key press?
+	case tea.KeyMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		// The "down" and "j" keys move the cursor down
+		case "down", "j":
+			if m.cursor < 11 {
+				m.cursor++
+			}
 		}
 	}
-	fmt.Println("hello")
+
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
+}
+
+func (m model) View() string {
+	s := ""
+	for i := 0; i <= m.cursor; i++ {
+		s += "\n"
+	}
+	s += "<>"
+	return s
+}
+
+func main() {
+	p := tea.NewProgram(newModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
 }
